ee28d62882f94a9e7b87861ea8007d5d8d713fde
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-01-18 18:19:04 -0800

    exercise 10

---
+++ week1/exercise10.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+from __future__ import print_function
+from ciscoconfparse import *
+import re
+
+
+parsedfile = CiscoConfParse("cisco_ipsec.txt")
+
+
+crypto_map = parsedfile.find_objects(r"^crypto map")
+
+no_aes_maps = []
+temp = []
+
+for each in crypto_map:
+    child = each.re_search_children(r"set transform-set (?!AES)")
+    if child:
+        temp.append(each)
+        temp.append(child)
+        no_aes_maps.append(temp)
+
+print(no_aes_maps)

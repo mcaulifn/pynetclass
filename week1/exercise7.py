a1f3b2faf08bc753eb7718c1a9a5ca9db402c451
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-01-18 18:51:26 -0800

    removed import __future__, changed CiscoConfParse to *

--- week1/exercise7.py
+++ week1/exercise7.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 
-#from __future__ import print_function
 import yaml, json
 from pprint import pprint
 
--- week1/exercise9.py
+++ week1/exercise9.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 
-from __future__ import print_function
-from ciscoconfparse import CiscoConfParse
+from ciscoconfparse import *
 
 
 parsedfile = CiscoConfParse("cisco_ipsec.txt")

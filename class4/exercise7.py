a171c3f0e47ca7d467f5845ff90ff702b039bbc0
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-02-09 17:10:53 -0800

    exercise 7 completed

---
+++ class4/exercise7.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+''' This module uses netmiko to run commands.'''
+
+from getpass import getpass
+import netmiko
+
+
+
+def main():
+    '''main'''
+    
+    rtr1 = {
+        'device_type': 'cisco_ios',
+        'ip': '50.76.53.27',
+        'port': 22,
+        'username': 'pyclass',
+        'password': getpass(),
+        }
+
+    rtr2 = {
+        'device_type': 'cisco_ios',
+        'ip': '50.76.53.27',
+        'port': 8022,
+        'username': 'pyclass',
+        'password': getpass(),
+        }
+
+    srx = {
+        'device_type': 'juniper',
+        'ip': '50.76.53.27',
+        'port': 9822,
+        'username': 'pyclass',
+        'password': getpass(),
+        }
+
+    routerlist = [rtr2]
+    confchanges = ['logging buffered 37788']
+
+    
+    for each in routerlist:
+        conn = netmiko.ConnectHandler(**each)
+    
+        print(conn.send_command('show run | i logging buff'))
+        conn.send_config_set(confchanges)
+        print(conn.send_command('show run | i logging buff'))
+        conn.disconnect()
+
+if __name__ == "__main__":
+    main()

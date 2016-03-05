408494e0bbe11a11f71cef32547f05d2968c4633
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-03-05 14:02:28 -0800

    exercise 1b

---
+++ class8/djproject/djproject/exercise1b.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python
+'''Assigns the correct credentials to the network devices'''
+
+from __future__ import print_function
+import django
+from net_system.models import NetworkDevice, Credentials
+
+
+def main():
+   '''main'''
+   
+   django.setup()
+   
+   for device in NetworkDevice.objects.all():
+        if device.device_type == 'cisco_ios':
+            device.credentials = Credentials.objects.get(username='pyclass')
+        elif device.device_type == 'juniper':
+            device.credentials = Credentials.objects.get(username='pyclass')
+        elif device.device_type == 'arista_eos':
+            device.credentials = Credentials.objects.get(username='admin1')
+        device.save()
+        print('%s : %s' %(device.device_name, device.credentials))
+
+if __name__ == "__main__":
+    main()

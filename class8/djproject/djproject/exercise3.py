cb17c78d591f01eef9e3fa24eae6aed54e64ead0
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-03-05 14:14:26 -0800

    exercise 3

---
+++ class8/djproject/djproject/exercise3.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+'''Adds new devices to db'''
+
+from __future__ import print_function
+import django
+from net_system.models import NetworkDevice
+
+def main():
+    '''main'''
+    django.setup()
+
+    pynet_rtrtest01 = NetworkDevice(
+        device_name='pynet-rtrtest01',
+        device_type='cisco_ios',
+        ip_address='50.76.53.27',
+        port=10022,
+    )
+    pynet_rtrtest01.save()
+
+    pynet_rtrtest02 = NetworkDevice.objects.get_or_create(
+        device_name='pynet-rtrtest02',
+        device_type='cisco_ios',
+        ip_address='50.76.53.27',
+        port=11022,
+    )
+
+if __name__ == "__main__":
+    main()

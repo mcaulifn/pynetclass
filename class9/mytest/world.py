8312f5421a6d0eb04c045f82b35466c2210d4e16
Nathaniel McAuliffe <mcaulifn@outlook.com>
2016-04-06 18:13:57 -0700

    added class for exercise 4

--- class9/mytest/world.py
+++ class9/mytest/world.py
@@ -2,6 +2,22 @@
 
 from __future__ import print_function
 
+class MyClass(object):
+    def __init__(self, var1, var2, var3):
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+
+    def hello(self):
+        print("Hello world")
+        print("%s, %s, and %s were the variables passed in." %(str(self.var1), str(self.var2), str(self.var3)))
+        return
+
+    def not_hello(self):
+        print("Goodbye") 
+        print("%s, %s, and %s were the variables passed in." %(str(self.var1), str(self.var2), str(self.var3)))
+        return
+
 def func1():
     print("This is func1 from world.py.")
 
